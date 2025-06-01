89b944fd1b562f00cdd8c82acf3ee28af03fd1bc
Hamid Ghanbari <70950300+hghanbari@users.noreply.github.com>
2025-06-01 14:59:51 +0200

    Start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Mantle of Praise</h1>
+        <p>
+          Under construction
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
