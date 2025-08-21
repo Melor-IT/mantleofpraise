6ee6d92d14b3afe47adb9addbcf3e48322097370
Hamid Ghanbari <70950300+hghanbari@users.noreply.github.com>
2025-08-21 22:44:21 +0200

    tetx

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -33,7 +33,19 @@ const messages = {
   aboutUsText:
     'The Spirit of the Lord GOD is upon me; for the LORD has anointed me to bring good news to the meek, to heal the brokenhearted, to proclaim liberty to captives and release to the bound; to declare the year of the LORD’s favor and the day of His vengeance; to comfort all who mourn, giving them beauty for ashes, joy for mourning, and a garment of praise for heaviness, that they may be called trees of righteousness, the planting of the LORD, that He may be glorified.',
   aboutUsTextOnder: '- Isaiah 61:1-3',
-
+  firstService: 'Equipping Worshipers',
+  firstServiceText:
+    'Preparing and training members for service in churches and leading worship sessions.',
+  secondService: 'Nurturing Prophets',
+  secondServiceText:
+    'Teaching, guiding, and supporting prophets for effective ministry in the body of Christ and prophetic flow. Worship combined with prophetic service and leading prayer sessions.',
+  thirdService: 'Organizing Prayer and Worship Sessions',
+  thirdServiceText:
+    'Combining music and worship under the guidance of the Holy Spirit, inspired by the Bible (1 Samuel 10:5), and organizing worship and prayer meetings to strengthen the presence of the Lord.',
+  more: 'More',
+  visen: 'Our Vision',
+  visenText:
+    'Our vision is to form an inspiring community of worshipers and prophets who, through music and worship filled with the guidance of the Holy Spirit, spread the light and presence of God in churches, awaken and heal every heart, and create a living, deep, and lasting experience of connection with the Lord for each individual and congregation.'
 };
 
 export default messages;
--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -33,6 +33,19 @@ const messages = {
     'Mijn God, ik roep des daags, en Gij antwoordt niet, en des nachts, en ik kom niet tot stilte. Nochtans zijt Gij de Heilige, die troont op de lofzangen Israëls. Op U hebben onze vaderen vertrouwd, zij hebben vertrouwd, en Gij deedt hen ontkomen;',
   aboutUsText:
     'De Geest van de Heere is op mij, want de HEERE heeft mij gezalfd om goede tijding te brengen aan de ootmoedigen, om de gebrokenen van hart te genezen, vrijheid uit te roepen voor gevangenen en opening van de gevangenis voor wie gebonden zijn; om het jaar van het welbehagen van de HEERE en de dag van de wraak van onze God te verkondigen; om allen die treuren te troosten, hun schoonheid voor as te geven, vreugde-olie voor rouw, en een kleed van lof voor een verslagen geest; opdat zij genoemd worden eiken van gerechtigheid, een planting van de HEERE, tot Zijn verheerlijking.',
-  aboutUsTextOnder: '- Jesaja 61:1-3'
+  aboutUsTextOnder: '- Jesaja 61:1-3',
+  firstService: 'Uitrusten van aanbidders',
+  firstServiceText:
+    'Voorbereiden en trainen van leden voor dienst in kerken en het leiden van aanbiddingssessies.',
+  secondService: 'Opvoeden van profeten',
+  secondServiceText:
+    'Onderwijzen, begeleiden en ondersteunen van profeten voor effectieve bediening in het lichaam van Christus en de profetische stroom. Aanbidding gecombineerd met profetische dienst en het leiden van gebedssessies.',
+  thirdService: 'Organiseren van gebeds- en aanbiddingssessies',
+  thirdServiceText:
+    'Combineren van muziek en aanbidding onder leiding van de Heilige Geest, geïnspireerd door de Bijbel (1 Samuël 10:5), en het organiseren van aanbidding- en gebedsbijeenkomsten om de aanwezigheid van de Heer te versterken.',
+  more: 'meer',
+  visen: 'Onze Visie',
+  visenText:
+    'Onze visie is het vormen van een inspirerende gemeenschap van aanbidders en profeten die, door middel van muziek en aanbidding vervuld met de leiding van de Heilige Geest, het licht en de aanwezigheid van God in kerken verspreiden, elk hart wekken en genezen, en een levendige, diepe en blijvende ervaring van verbinding met de Heer creëren voor elk individu en elke gemeente.'
 };
 export default messages;
